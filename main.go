81ac16fb0f57a5efbecd6cd96c025606d7b26b7c
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2022-06-05 17:26:41 +0430

    comments

--- main.go
+++ main.go
@@ -1,11 +1,19 @@
 package main
 
+//Command-line arguments are a common way to parameterize execution of programs.
+// For example,
+// go run hello.go uses run and hello.go arguments to the go program.
+
 import (
 	"fmt"
 	"os"
 )
 
 func main() {
+
+	//os.Args provides access to raw command-line arguments.
+	//Note that the first value in this slice is the path to the program,
+	//and os.Args[1:] holds the arguments to the program.
 
 	argsWithProg := os.Args
 	argsWithoutProg := os.Args[1:]
@@ -16,3 +24,14 @@ func main() {
 	fmt.Println(argsWithoutProg)
 	fmt.Println(arg)
 }
+
+//we can get individual args with normal indexing.
+
+//To experiment with command-line arguments it’s best to build a binary with go build first.
+
+//go build main.go  for output :
+
+//$ ./command-line-arguments a b c d
+//[./command-line-arguments a b c d]
+//[a b c d]
+//c
